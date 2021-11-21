20336190ba3b62ff55184bc975bac3fa359a4aff
GNGitHub20 <76264078+GNGitHub20@users.noreply.github.com>
2021-11-22 01:22:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,10 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+BUILD_DIR=$1
+
+# Exit early if app is clearly not Python.
+if [ ! -f "$BUILD_DIR/requirements.txt" ] && [ ! -f "$BUILD_DIR/setup.py" ] && [ ! -f "$BUILD_DIR/Pipfile" ]; then
+  exit 1
+fi
